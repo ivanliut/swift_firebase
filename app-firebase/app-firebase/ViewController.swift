a3fb6c157d77f1477d0e57284d734250120f58ee
administrator <iv>
2019-05-10 21:06:35 +0300

    initial setup

---
+++ app-firebase/app-firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-firebase
+//
+//  Created by administrator on 5/10/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
