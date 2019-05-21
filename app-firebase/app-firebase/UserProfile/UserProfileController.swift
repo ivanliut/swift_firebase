f675762e72d73111c60cea524c600a949b0b7517
administrator <iv>
2019-05-22 00:40:53 +0300

    modify header of collection view

---
+++ app-firebase/app-firebase/UserProfile/UserProfileController.swift
@@ -0,0 +1,59 @@
+//
+//  UserProfileController.swift
+//  app-firebase
+//
+//  Created by administrator on 5/16/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        fetchUser()
+        collectionView.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "headerId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath) as! UserProfileHeader
+        
+        header.user = self.user
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+
+    var user: User?
+    fileprivate func fetchUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            print(snapshot.value ?? "")
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            
+            self.user = User(dictionary: dictionary)
+            
+            self.navigationItem.title = self.user?.username
+            
+            self.collectionView.reloadData()
+            
+        }) { (err) in
+            print("Failed to fetch users:", err)
+        }
+    }
+
+}
+
+struct User {
+    let username: String
+    let profileImageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    }
+}
---
+++ app-firebase/app-firebase/UserProfile/UserProfileHeader.swift
@@ -0,0 +1,88 @@
+//
+//  UserProfileHeader.swift
+//  app-firebase
+//
+//  Created by administrator on 5/20/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileHeader: UICollectionViewCell {
+    
+    let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        return iv
+    }()
+    
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "grid"), for: .normal)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "list"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "bookmark"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(profileImageView)
+        profileImageView.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        profileImageView.clipsToBounds = true
+        
+        setupBottomToolbar()
+    }
+    
+    fileprivate func setupBottomToolbar() {
+        let stackView = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        addSubview(stackView)
+        stackView.anchor(top: nil, left: leftAnchor, bottom: self.bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+    }
+    
+    var user: User? {
+        didSet {
+            setupProfileImage()
+        }
+    }
+    
+    fileprivate func setupProfileImage() {
+        guard let profileImageUrl = user?.profileImageUrl else {
+            return
+        }
+        
+        guard let url = URL(string: profileImageUrl ) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch profile image:", err)
+                return
+            }
+            guard let data = data else { return }
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.profileImageView.image = image
+            }
+            
+            }.resume()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
