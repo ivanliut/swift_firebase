79ef205de423252087ce69c630ec991a96849ef9
administrator <iv>
2019-06-08 18:03:55 +0300

    connect photo managerto collection view cells

---
+++ app-firebase/app-firebase/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoSelectorCell.swift
+//  app-firebase
+//
+//  Created by administrator on 6/8/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
