cf01570c82e1d744a5fb1e35c173bf33f1a49857
administrator <iv>
2019-06-10 23:23:02 +0300

    add functionality to next navigation button

---
+++ app-firebase/app-firebase/AddPhoto/SharePhotoController.swift
@@ -0,0 +1,54 @@
+//
+//  SharePhotoController.swift
+//  app-firebase
+//
+//  Created by administrator on 6/10/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import UIKit
+
+class SharePhotoController: UIViewController {
+    
+    var selectedImage: UIImage? {
+        didSet {
+            self.imageView.image = selectedImage
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
+        
+        setupImageAndTextViews()
+    }
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    fileprivate func setupImageAndTextViews() {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        view.addSubview(containerView)
+        containerView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
+        
+        containerView.addSubview(imageView)
+        imageView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 8, paddingRight: 0, width: 84, height: 0)
+    }
+    
+    @objc func handleShare() {
+        print("Sharing photo")
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
